f8bb429ef7b189e62b2140dab387b15a81b5fb42
Ignacio-Us <i.uribe01@ufromail.cl>
2024-04-16 15:35:44 -0400

    Implementación validadores en js

--- controlador.js
+++ controlador.js
@@ -1,37 +1,38 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
 
     const form = document.getElementById('form');
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
 
         const nombre = form.elements['name'].value;
         const email = form.elements['email'].value;
         const telephone = form.elements['telephone'].value;
 
-        if (formIsValid(nombre, email, telephone)){
+        if (formIsValid(nombre, email, telephone)) {
             alert('El formulario se a procesado de forma exitosa')
-        }else{
+        } else {
             alert('Hubo un problema para procesar los datos el formulario, por favor revise todos los campos')
         }
-
     });
 });
 
-function formIsValid(name,email,tel){
-    return nameIsValid(name) && emailIsValid(email) && telephoneIsValid(tel) ;
+function formIsValid(name, email, tel) {
+    console.log(nameIsValid(name));
+    console.log(emailIsValid(email));
+    console.log(telephoneIsValid(tel));
+    return nameIsValid(name) && emailIsValid(email) && telephoneIsValid(tel);
 }
 
-function nameIsValid(name){
+function nameIsValid(name) {
     let numChar = name.length;
     return numChar <= 20;
 }
 
-function emailIsValid(email){
-
+function emailIsValid(email) {
+    let atSign = email.include('@');
+    return atSign;
 }
 
-function telephoneIsValid(tel){
-
-
-
+function telephoneIsValid(tel) {
+    return (tel.startsWith('+569')) && ((tel.length - 4) = 8);
 }
